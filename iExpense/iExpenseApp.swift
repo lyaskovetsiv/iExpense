7c937ca5fb06a07c83e0262235369f67e095ac8e
Иван Лясковец <lyaskovets.iv@MacBook-Air-Ivan.local>
2023-08-03 20:04:38 +0300

    Initial commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Иван Лясковец on 03.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
